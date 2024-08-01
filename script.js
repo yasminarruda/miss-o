ba89964b671173a33b23f5f6f97233a6ecea1abf
yasminarruda <108303633+yasminarruda@users.noreply.github.com>
2024-08-01 11:51:53 -0300

    codigo

---
+++ script.js
@@ -0,0 +1,14 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const caixaResultado = document.querySelector(".caixa-resultado");
+
+const perguntas = [
+    {
+        enunciado:
+        alternativas: [
+            
+        ]
+    }
+]
